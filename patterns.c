a89e99fd681954b6585eac6ab564619ef6661b71
gourigs <71514229+gourigs@users.noreply.github.com>
2021-05-18 21:43:01 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -3,8 +3,8 @@
 int main(void)
 {
    int x,y;
-   x= get_int("Enter type of pattern(1 or 2) ");
-   y= get_int("Number of rows ");
+   x= get_int("Enter type of pattern(1 or 2) \n");
+   y= get_int("Number of rows \n");
    if(x==1)
    for(int i=1;i<=y;i++)
    {
